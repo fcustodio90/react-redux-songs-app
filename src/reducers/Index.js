974c5ba03b5cc19850c92a783c00b3f4addaed5b
filipe custodio <filipe.l.custodio@gmail.com>
2019-07-04 05:00:45 +0100

    Creating songs and selectedSong reducers

---
+++ src/reducers/Index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+  return [
+    { title: 'No scrubs', duration: '4:05' },
+    { title: 'Orion', duration: '8:05' },
+    { title: 'Santeria', duration: '3:23' },
+    { title: 'Lose yourself', duration: '5:33' }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null , action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
